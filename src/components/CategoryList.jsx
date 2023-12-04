709814871ce58644c58dcd1eaec81bfd6c6c4cc0
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-04 17:45:01 +0100

    add category page

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import { imgs, categories } from '../data';
+import { CategoryCard } from './CategoryCard';
 
 const [
 	imgPython,
@@ -14,119 +14,66 @@ const [
 export const CategoryList = () => {
 	return (
 		<div className='flex flex-row flex-wrap justify-center gap-4 mt-10'>
-      {/* Category Python */}
-			<Link
-				to={`/category/${categories.python}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgPython}
-						alt={`Category ${categories.python}`}
-						className='w-36'
-					/>
-				</div>
+			{/* Category Python */}
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.python}
-				</h1>
-			</Link>
-      {/* Category JavaScript*/}
-			<Link
-				to={`/category/${categories.javascript}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgJavaScript}
-						alt={`Category ${categories.javascript}`}
-						className='w-36'
-					/>
-				</div>
+			<CategoryCard
+				category={categories.python}
+				src={imgPython}
+				alt={`Category ${categories.python}`}
+				gradientColor='from-blue-500 to-purple-500'
+			/>
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.javascript}
-				</h1>
-			</Link>
-      {/* Category React */}
-			<Link
-				to={`/category/${categories.react}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgReact}
-						alt={`Category ${categories.react}`}
-						className='w-36'
-					/>
-				</div>
+			{/* Category JavaScript*/}
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.react}
-				</h1>
-			</Link>
-     
-      {/* Category NextJS */}
-			<Link
-				to={`/category/${categories.nextjs}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgNextJS}
-						alt={`Category ${categories.nextjs}`}
-						className='w-36'
-					/>
-				</div>
+			<CategoryCard
+				category={categories.javascript}
+				src={imgJavaScript}
+				alt={`Category ${categories.javascript}`}
+				gradientColor='from-blue-500 to-purple-500'
+			/>
+			{/* Category React */}
+			<CategoryCard
+				category={categories.react}
+				src={imgReact}
+				alt={`Category ${categories.react}`}
+				gradientColor='from-blue-500 to-purple-500'
+			/>
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.nextjs}
-				</h1>
-			</Link>
-      {/* Category Html */}
-			<Link
-				to={`/category/${categories.html}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgHtml}
-						alt={`Category ${categories.html}`}
-						className='w-36'
-					/>
-				</div>
+			{/* Category NextJS */}
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.html}
-				</h1>
-			</Link>
-      {/* Category Css */}
-			<Link
-				to={`/category/${categories.css}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgCss}
-						alt={`Category ${categories.css}`}
-						className='w-36'
-					/>
-				</div>
+			<CategoryCard
+				category={categories.nextjs}
+				src={imgNextJS}
+				alt={`Category ${categories.nextjs}`}
+				gradientColor='from-blue-500 to-purple-500'
+			/>
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.css}
-				</h1>
-			</Link>
-       {/* Category TypeScript */}
-			<Link
-				to={`/category/${categories.typescript}`}
-				className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 transition-all hover:scale-105'>
-				<div className='p-5 flex justify-center items-center'>
-					<img
-						src={imgTypeScript}
-						alt={`Category ${categories.typescript}`}
-						className='w-36'
-					/>
-				</div>
+			{/* Category Html */}
 
-				<h1 className='text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-					{categories.typescript}
-				</h1>
-			</Link>
+			<CategoryCard
+				category={categories.html}
+				src={imgHtml}
+				alt={`Category ${categories.html}`}
+				gradientColor='from-blue-500 to-purple-500'
+			/>
+
+			{/* Category Css */}
+
+			<CategoryCard
+				category={categories.css}
+				src={imgCss}
+				alt={`Category ${categories.css}`}
+				gradientColor='from-blue-500 to-purple-500'
+			/>
+
+			{/* Category TypeScript */}
+
+			<CategoryCard
+				category={categories.typescript}
+				src={imgTypeScript}
+				alt={`Category ${categories.typescript}`}
+				gradientColor='from-gray-200 to-gray-500'
+			/>
 		</div>
 	);
 };
--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { questions } from '../data';
+
+const shuffleArray = array => {
+	const newArray = array.sort(() => Math.random() - 0.5);
+	return newArray.slice(0, 5);
+};
 
 export const CategoryPage = () => {
-  return (
-    <div>CategoryPage</div>
-  )
-}
+	const { category } = useParams();
+
+	const [questionsFiltered, setQuestionsFiltered] = useState(
+		questions.filter(question => question.category === category)
+	);
+
+	useEffect(() => {
+		const newQuestions = shuffleArray(questionsFiltered);
+		setQuestionsFiltered(newQuestions);
+	}, [questionsFiltered]);
+
+	return (
+		<div
+			className='container flex flex-col items-center justify-center gap-10'
+			style={{ height: 'calc(100vh - 5rem)' }}>
+			CategoryPage
+		</div>
+	);
+};
