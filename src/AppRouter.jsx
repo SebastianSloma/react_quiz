ecbf399b36a79b69044efea0077f61a6e089d8b7
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-04 09:23:34 +0100

    added routes to approuter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage, CategoryPage } from './pages';
 
 const AppRouter = () => {
-  return (
-    <div>AppRouter</div>
-  )
-}
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/category/:category' element={<CategoryPage />} />
+			</Routes>
+		</>
+	);
+};
 
-export default AppRouter
+export default AppRouter;
