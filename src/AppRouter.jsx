7080d843fe2ad74829780d49a0ba8078445cafe1
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-04 09:49:47 +0100

    added navbar

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage, CategoryPage } from './pages';
+import { NavBar } from './components/NavBar';
 
 const AppRouter = () => {
 	return (
 		<>
+        <NavBar/>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/category/:category' element={<CategoryPage />} />
