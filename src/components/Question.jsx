b8db4da71ca6c75eef8a0ad60978fa6518cc669c
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-05 11:19:18 +0100

    added hooks part 1

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,45 +1,70 @@
-export const Question = () => {
+import { useState, useEffect } from 'react';
+
+export const Question = ({
+	filteredQuestion,
+	questionsFiltered,
+	indexQuestion,
+	setIndexQuestion,
+	setActiveQuiz,
+}) => {
+	const [answersRandom, setAnswersRandom] = useState([]);
+
+	useEffect(() => {
+		const answers = [
+			...filteredQuestion.incorrect_answers,
+			filteredQuestion.correct_answer,
+		];
+
+		setAnswersRandom(answers.sort(()=>Math.random()-0.5));
+	}, [filteredQuestion]);
+
+	const onNextQuestion = () => {
+		setIndexQuestion(indexQuestion + 1);
+	};
+
 	return (
 		<>
 			<div className='flex flex-col justify-between shadow-md shadow-slate-300 w-[600px] h-[600px] p-10 rounded-lg'>
 				<div className='flex justify-between'>
 					<span className='text-xl font-bold'>
-                        {/* Number actual question and number all questions */}
-
-                    </span>
-                    <div>
-                        <span className='font-semibold'>
-                            Difficult:
-                        </span>
-                        <span className='font-bold'>
-                            {/* Difficult of a question */}
-
-                        </span>
-                    </div>
+						{/* Number actual question and number all questions */}
+						{indexQuestion + 1} / {questionsFiltered.length}
+					</span>
+					<div>
+						<span className='font-semibold'>Difficult: </span>
+						<span className='font-bold'>
+							{/* Difficult of a question */}
+							{filteredQuestion.difficulty}
+						</span>
+					</div>
 				</div>
 
-                <button className='border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900'>
-                    Restart
-                </button>
-                <div>
-                    <h1 className='font-bold'>Question</h1>
-                </div>
+				<button
+					className='border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900'
+					onClick={() => setActiveQuiz(false)}>
+					Restart
+				</button>
+				<div>
+					<h1 className='font-bold'>{filteredQuestion.question}</h1>
+				</div>
 
-                {/* answers */}
-                <div className='grid grid-cols-2 gap-5'>
-                {/* map the layout of good and bad answers */}
-                <button className="border-p-5 rounded-lg flex justify-center items-center hover:scale-105">
-                    <p className="font-medium text-center-text sm">
-                    Any response
-                    </p>
-                </button>
-                </div>
-                {/* Conditional to show the next question button at the end of the question */}
-                <button className="border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium">
-                Finish or next question
-                </button>
-
-
+				{/* answers */}
+				<div className='grid grid-cols-2 gap-5'>
+					{/* map the layout of good and bad answers */}
+					{answersRandom.map((answer, index) => (
+						<button
+							className='border p-5 rounded-lg flex justify-center items-center hover:scale-105 '
+							key={answer}>
+							<p className='font-medium text-center-text sm'>{answer}</p>
+						</button>
+					))}
+				</div>
+				{/* Conditional to show the next question button at the end of the question */}
+				<button
+					className='border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium'
+					onClick={onNextQuestion}>
+					Finish or next question
+				</button>
 			</div>
 		</>
 	);
--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { questions } from '../data';
+import { questions, imgs } from '../data';
 import { Question } from '../components/Question';
 
 const shuffleArray = array => {
@@ -9,22 +9,58 @@ const shuffleArray = array => {
 };
 
 export const CategoryPage = () => {
+	// Read URL parameter
 	const { category } = useParams();
+
+	const [imgCategory] = imgs.filter(
+		img => img === `/src/assets/${category.toLowerCase()}.png`
+	);
 
 	const [questionsFiltered, setQuestionsFiltered] = useState(
 		questions.filter(question => question.category === category)
 	);
+	// const [questionsFiltered, setQuestionsFiltered] = useState(
+	// 	questions.filter(question => question.category === category)
+
+	const [indexQuestion, setIndexQuestion] = useState(0);
+	const [activeQuiz, setActiveQuiz] = useState(false);
 
 	useEffect(() => {
 		const newQuestions = shuffleArray(questionsFiltered);
 		setQuestionsFiltered(newQuestions);
-	}, [questionsFiltered]);
-
+	}, []);
+	// ?????????????????
 	return (
 		<div
 			className='container flex flex-col items-center justify-center gap-10'
 			style={{ height: 'calc(100vh - 5rem)' }}>
-			<Question/>
+			{activeQuiz ? (
+				<Question
+					filteredQuestion={questionsFiltered[indexQuestion]}
+					setIndexQuestion={setIndexQuestion}
+					indexQuestion={indexQuestion}
+					questionsFiltered={questionsFiltered}
+					setActiveQuiz={setActiveQuiz}
+				/>
+			) : (
+				<>
+					<div className='flex flex-col gap-5'>
+						<h1 className='text-3xl text-teal-900 text-center font-bold'>
+							{category}
+						</h1>
+
+						<div className='flex justify-center items-center'>
+							<img src={imgCategory} alt={category} className='w-72' />
+						</div>
+					</div>
+
+					<button
+						className='text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all hover:bg-yellow-500 hover:text-gray-900'
+						onClick={() => setActiveQuiz(true)}>
+						Start Quiz
+					</button>
+				</>
+			)}
 		</div>
 	);
 };
